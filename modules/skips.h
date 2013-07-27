541a34e2e333d1ebd449f8ebf1c52b42d6409aa5
rthompson <rthompson@physics.unlv.edu>
2013-07-27 13:28:42 -0700

    BUGFIX - reader was skipping the AGE block when no stars existed.

--- modules/skips.h
+++ modules/skips.h
@@ -160,7 +160,7 @@ void skip_blocks(int blockval){
 
   // AGE
   if(blockval==10) return;
-  if(header.flag_stellarage){
+  if(header.flag_stellarage && header.npart[4]>0){
     fread(&skip1,sizeof(int),1,infp);
     fseek(infp,header.npart[4]*sizeof(float),SEEK_CUR);
     fread(&skip2,sizeof(int),1,infp);
