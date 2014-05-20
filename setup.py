d8b3e7918d3baf8833d8fa2de6df3ab504bc74f0
Robert Thompson <rthompson@physics.unlv.edu>
2014-05-20 13:38:35 +0200

    removing HDF5 library include by default.

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@
 
 from numpy.distutils.core import setup, Extension
 
-HDF5INCL = "/Users/bob/local/hdf5/include"
-HDF5LIB  = "/Users/bob/local/hdf5/lib"
+HDF5_PRESENT = 0
+HDF5INCL     = "/Users/bob/local/hdf5/include"
+HDF5LIB      = "/Users/bob/local/hdf5/lib"
 
 NAME = "readgadget"
 FILE = "readgadget.c"
@@ -11,9 +12,14 @@ FILE = "readgadget.c"
 #ext = Extension(NAME,[FILE],
 #                include_dirs = [HDF5INCL])
 
-setup(name = "readgadget",
-      version="0.5",
-      ext_modules=[Extension("readgadget",["readgadget.c"], 
-                             include_dirs = [HDF5INCL],
-                             libraries    = ['hdf5'],
-                             library_dirs = [HDF5LIB])])
+if HDF5_PRESENT:
+    setup(name = "readgadget",
+          version="0.5",
+          ext_modules=[Extension("readgadget",["readgadget.c"], 
+                                 include_dirs = [HDF5INCL],
+                                 libraries    = ['hdf5'],
+                                 library_dirs = [HDF5LIB])])
+else:
+    setup(name = "readgadget",
+          version="0.5",
+          ext_modules=[Extension("readgadget",["readgadget.c"])])    
