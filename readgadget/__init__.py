a55594b5f48ed919f9b70acbf340f7a6a87276d7
Robert Thompson <rthompson@physics.unlv.edu>
2014-10-05 01:30:01 +0200

    added initial P-Star support.

--- readgadget/__init__.py
+++ readgadget/__init__.py
@@ -2,4 +2,4 @@ from readgadget import readsnap,readhead
 from readrockstar import readrockstar
 from readrockstar import readrockstargalaxies
 from readfofspecial import readfofspecial
-
+from readpstar import readpstar
--- readgadget/readpstar.py
+++ readgadget/readpstar.py
@@ -7,13 +7,13 @@ class Group(object):
         self.index = index
         self.npart_total = npart
 
-def readfofspecial(catdir,snapnum,groupIndex,**kwargs):
-    """Read and return info from FOFSpecial catalogues.
+def readpstar(catdir,snapnum,groupIndex,**kwargs):
+    """Read and return info from P-Star catalogues.
 
     Parameters
     ----------
     catdir : string
-        path to your FOF catalogues
+        path to your PSTAR catalogues
     snapnum : int
         snapnum you are interested in
     groupIndex : int
@@ -26,6 +26,72 @@ def readfofspecial(catdir,snapnum,groupIndex,**kwargs):
 
     GROUPS = []
 
+    fcat   = open('%s/catalogue_%03d' % (catdir,snapnum),'rb')
+    fprop  = open('%s/properties_%03d' % (catdir,snapnum),'rb')
+    fpos   = open('%s/pos_%03d' % (catdir,snapnum),'rb')
+    fptype = open('%s/type_%03d' % (catdir,snapnum),'rb')
+    findex = open('%s/index_%03d' % (catdir,snapnum),'rb')
+    
+    ngroups  = np.fromfile(fcat,dtype=np.uint32,count=1)[0]
+    nparttot = np.fromfile(fpos,dtype=np.uint32,count=1)[0]
+    fprop.seek(4,1)
+    fptype.seek(4,1)
+    findex.seek(4,1)
+
+    for i in range(0,ngroups):
+        gpids = []
+        spids = []
+
+        nparts = np.fromfile(fcat,dtype=np.uint32,count=1)[0]
+        offset = np.fromfile(fcat,dtype=np.uint32,count=1)[0]
+        for j in range(0,nparts):
+            ppos  = np.fromfile(fpos,dtype=np.float32,count=3)
+            ptype = np.fromfile(fptype,dtype=np.uint32,count=1)[0]
+            pid   = np.fromfile(findex,dtype=np.uint32,count=1)[0]
+            if ptype == 0:
+                gpids.append(pid)
+            elif ptype == 4:
+                spids.append(pid)
+            
+        pmstars = np.fromfile(fprop,dtype=np.float32,count=1)[0]
+        mags    = np.fromfile(fprop,dtype=np.float32,count=4)
+        pcm     = np.fromfile(fprop,dtype=np.float32,count=3)
+        pmsfr   = np.fromfile(fprop,dtype=np.float32,count=1)[0]
+        pmgas   = np.fromfile(fprop,dtype=np.float32,count=1)[0]
+        pmmetals= np.fromfile(fprop,dtype=np.float32,count=1)[0]
+        pmgmetals=np.fromfile(fprop,dtype=np.float32,count=1)[0]
+
+        GROUPS.append(Group(nparts,i))
+        GROUPS[i].mstar = pmstars
+        GROUPS[i].mgas  = pmgas
+        GROUPS[i].cm    = pcm
+        GROUPS[i].metals= pmmetals
+        GROUPS[i].gmetals=pmgmetals
+        GROUPS[i].gpids = gpids
+        GROUPS[i].spids = spids
+
+    fcat.close()
+    fprop.close()
+    fpos.close()
+    fptype.close()
+    findex.close()
+
+    if groupIndex == -1:
+        groupIndex = range(0,ngroups)
+
+    if isinstance(groupIndex,int):
+        grp = GROUPS[groupIndex]
+        return grp
+        
+    elif isinstance(groupIndex,list):
+        grps = []
+        for i in range(0,len(groupIndex)):
+            grp = GROUPS[groupIndex[i]]
+            grps.append(grp)
+
+        return grps
+
+    """
     ## group catelog
     f = open('%s/fof_special_catalogue_%03d' % (catdir,snapnum),'rb')
     ngroups = np.fromfile(f,dtype=np.int32,count=1)[0]
@@ -63,9 +129,6 @@ def readfofspecial(catdir,snapnum,groupIndex,**kwargs):
     indexList = np.fromfile(f,dtype=np.uint32,count=nindexes)
     f.close()
 
-    if groupIndex == -1:
-        groupIndex = range(0,ngroups)
-
     if isinstance(groupIndex,int):
         grp = GROUPS[groupIndex]
         grp.indexes = np.zeros(grp.npart_total,dtype=np.uint32)
@@ -83,4 +146,4 @@ def readfofspecial(catdir,snapnum,groupIndex,**kwargs):
             for j in range(0,grp.npart_total):
                 grp.indexes[j] = indexList[grp.cumcount + j] - 1
         return grps
-            
+    """
